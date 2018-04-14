36b52303a9a2287f7c13f0ce2cc4e47ff6cc9fe6
JuanfBg <bgjuan11@gmail.com>
2018-04-14 14:43:05 -0500

    First Version

---
+++ CardView/app/src/main/java/co/edu/konradlorenz/cardview/AlbumDetailActivity.java
@@ -0,0 +1,31 @@
+package co.edu.konradlorenz.cardview;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class AlbumDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album_detail);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album_detail);
+        ImageView img = findViewById(R.id.imagenAlbum);
+
+        Intent intent= getIntent();
+        Bundle b =intent.getExtras();
+
+        if(b!=null){
+
+            img.setImageResource(b.getInt("IMG"));
+        }
+
+
+
+    }
+}
